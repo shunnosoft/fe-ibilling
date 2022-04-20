5e692ccc2927ff173d4b6eb4aaf2535de27fc303
Shahadat Hossain <shahadat.cseru@gmail.com>
2022-04-21 05:37:14 +0600

    color changed

--- src/pages/public-pages/Home.jsx
+++ src/pages/public-pages/Home.jsx
@@ -9,27 +9,7 @@ export default function Landing() {
       <div className="landingWrapper">
         <div className="container-fluide landingContainer">
           {/* <video src="img/v3.mp4" muted loop autoplay type="mp4"></video> */}
-          <div className="textBox">
-            <div className="landingMain">
-              <div className="landinglogodiv">
-                <img
-                  className="landingLogonew"
-                  src="./assets/img/logo.png"
-                  alt=""
-                />
-              </div>
 
-              <div className="buttons">
-                <NavLink to="/login">
-                  <p className="goToLoginPage custom-btn">লগইন</p>
-                </NavLink>
-                <NavLink to="/register">
-                  <p className="goToLoginPage custom-btn"> সাইন আপ</p>
-                </NavLink>
-              </div>
-              {/* <h2 className="LandingTitle">নেটফি</h2> */}
-            </div>
-          </div>
           <div className="textBox2">
             <p className="intro1">
               বর্তমান বিশ্বে যোগাযোগ ব্যবস্থার উন্নতি ও অগ্রগতিতে অন্যতম অবদান
@@ -102,6 +82,28 @@ export default function Landing() {
             </ul>
 
             <FooterLink></FooterLink>
+          </div>
+
+          <div className="textBox">
+            <div className="landingMain">
+              <div className="landinglogodiv">
+                <img
+                  className="landingLogonew"
+                  src="./assets/img/logo.png"
+                  alt=""
+                />
+              </div>
+
+              <div className="buttons">
+                <NavLink to="/login">
+                  <p className="goToLoginPage custom-btn">লগইন</p>
+                </NavLink>
+                <NavLink to="/register">
+                  <p className="goToLoginPage custom-btn"> সাইন আপ</p>
+                </NavLink>
+              </div>
+              {/* <h2 className="LandingTitle">নেটফি</h2> */}
+            </div>
           </div>
         </div>
       </div>
