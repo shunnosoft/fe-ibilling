f809acc2f078322e4725a2b877f2491a831fd2ac
NahidJHN <jubaidhossain66@gmail.com>
2022-06-16 21:28:47 +0600

    bill cycle month name modify to 3 letter

--- src/pages/staticCustomer/StaticCustomer.jsx
+++ src/pages/staticCustomer/StaticCustomer.jsx
@@ -461,7 +461,7 @@ export default function Customer() {
         Header: "বিল সাইকেল",
         accessor: "billingCycle",
         Cell: ({ cell: { value } }) => {
-          return moment(value).format("MMMM DD YYYY hh:mm A");
+          return moment(value).format("MMM DD YYYY hh:mm A");
         },
       },
 
