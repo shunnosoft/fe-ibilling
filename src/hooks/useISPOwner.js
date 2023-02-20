33b0bbeef76eeb531c00c2a44ac89fffc52c8463
NahidJHN <jubaidhossain66@gmail.com>
2023-02-20 22:21:09 +0600

    after login redirect problem fixed

--- src/hooks/useISPOwner.js
+++ src/hooks/useISPOwner.js
@@ -5,24 +5,27 @@ const useISPowner = () => {
   const ispOwner = useSelector(
     (state) => state.persistedReducer.auth.ispOwnerData
   );
-
+  const bpSettings = useSelector(
+    (state) => state.persistedReducer.auth?.userData?.bpSettings
+  );
   const [ispOwnerId, setIspOwnerId] = useState(ispOwner.id);
-  const [hasMikrotik, setHasMikrotik] = useState(
-    ispOwner.bpSettings?.hasMikrotik
-  );
-  const [hasReseller, setHasReseller] = useState(
-    ispOwner.bpSettings?.hasReseller
-  );
+  const [hasMikrotik, setHasMikrotik] = useState(bpSettings?.hasMikrotik);
+  const [hasReseller, setHasReseller] = useState(bpSettings?.hasReseller);
   const [companyName, setCompanyName] = useState(ispOwner?.company);
 
   useEffect(() => {
     if (ispOwner) {
       setIspOwnerId(ispOwner.id);
-      setHasMikrotik(ispOwner.bpSettings?.hasMikrotik);
-      setHasReseller(ispOwner.bpSettings?.hasReseller);
       setCompanyName(ispOwner.company);
     }
   }, [ispOwner]);
+
+  useEffect(() => {
+    if (bpSettings) {
+      setHasMikrotik(bpSettings?.hasMikrotik);
+      setHasReseller(bpSettings?.hasReseller);
+    }
+  }, [bpSettings]);
 
   return { ispOwnerId, hasMikrotik, hasReseller, companyName };
 };
