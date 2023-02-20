5a0a693bb62d96e9966975af8daf7de0342a9590
bashhojaifa <b.hojaifa@gmail.com>
2023-02-20 22:16:41 +0600

    uncomment production api at apilink

--- src/api/apiLink.js
+++ src/api/apiLink.js
@@ -3,7 +3,7 @@ import jwt_decode from "jwt-decode";
 import { userLogout } from "../features/actions/authAsyncAction";
 
 // PRODUCTION
-// const BASE_URL = "https://netfeebd.net/api/v1/";
+const BASE_URL = "https://netfeebd.net/api/v1/";
 
 // Bashar
 // const BASE_URL = "http://192.168.1.11:3030/api/v1/";
@@ -12,7 +12,7 @@ import { userLogout } from "../features/actions/authAsyncAction";
 // const BASE_URL = "http://192.168.1.10:3030/api/v1/";
 
 // nahid
-const BASE_URL = "http://192.168.1.27:3030/api/v1/";
+// const BASE_URL = "http://192.168.1.27:3030/api/v1/";
 
 // LOCAL vai
 // const BASE_URL = "http://192.168.1.33:3030/api/v1/";
