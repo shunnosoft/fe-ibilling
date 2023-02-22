dc6b118ce929aa75be17d5d106c483d5cb96af7c
NahidJHN <jubaidhossain66@gmail.com>
2023-02-22 22:05:24 +0600

    working with customer sync and table toggle column show hide functional

--- src/pages/configMikrotik/configMikrotikModals/CustomerSync.jsx
+++ src/pages/configMikrotik/configMikrotikModals/CustomerSync.jsx
@@ -3,6 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../../components/common/Loader";
 import { fetchMikrotikSyncUser } from "../../../features/apiCalls";
 import { useTranslation } from "react-i18next";
+import apiLink from "../../../api/apiLink";
+import Table from "../../../components/table/Table";
+import IndeterminateCheckbox from "../../../components/table/bulkCheckbox";
+import { badge } from "../../../components/common/Utils";
+import moment from "moment";
 
 const CustomerSync = ({
   mikrotikId,
@@ -16,9 +21,14 @@ const CustomerSync = ({
 
   // loading state
   const [isLoading, setIsloading] = useState(false);
+  const [customer, setCustomer] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [firstLoading, setFirstLoading] = useState(false);
+  // bulk customer state
+  const [bulkCustomers, setBulkCustomer] = useState([]);
 
   // Sync Customer
-  const syncCostomer = () => {
+  const syncCustomer = () => {
     // send data for api
     const data = {
       ispOwner: ispOwner,
@@ -29,6 +39,103 @@ const CustomerSync = ({
     fetchMikrotikSyncUser(dispatch, data, setIsloading);
   };
 
+  const syncCustomerFirstTime = async () => {
+    setFirstLoading(true);
+    try {
+      const { data } = await apiLink({
+        method: "GET",
+        url: `/mikrotik/customer/${ispOwner}/${mikrotikId}?inActiveCustomer=${inActiveCustomer}?isSelected=${false}`,
+      });
+
+      setCustomer(data);
+      setLoaded(true);
+    } catch (error) {
+      console.log(error);
+    }
+    setFirstLoading(false);
+  };
+
+  const column = React.useMemo(
+    () => [
+      {
+        id: "selection",
+        Header: ({ getToggleAllPageRowsSelectedProps }) => (
+          <IndeterminateCheckbox
+            customeStyle={true}
+            {...getToggleAllPageRowsSelectedProps()}
+          />
+        ),
+        Cell: ({ row }) => (
+          <div>
+            <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+          </div>
+        ),
+        width: "2%",
+      },
+      {
+        width: "8%",
+        Header: t("id"),
+        accessor: "customerId",
+      },
+      {
+        width: "9%",
+        Header: t("name"),
+        accessor: "name",
+      },
+      {
+        width: "9%",
+        Header: t("PPPoE"),
+        accessor: "pppoe.name",
+      },
+      {
+        width: "12%",
+        Header: t("mobile"),
+        accessor: "mobile",
+      },
+
+      {
+        width: "8%",
+        Header: t("status"),
+        accessor: "status",
+        Cell: ({ cell: { value } }) => {
+          return badge(value);
+        },
+      },
+      {
+        width: "9%",
+        Header: t("paymentStatus"),
+        accessor: "paymentStatus",
+        Cell: ({ cell: { value } }) => {
+          return badge(value);
+        },
+      },
+      {
+        width: "9%",
+        Header: t("package"),
+        accessor: "pppoe.profile",
+      },
+      {
+        width: "8%",
+        Header: t("mountly"),
+        accessor: "monthlyFee",
+      },
+      {
+        width: "9%",
+        Header: t("balance"),
+        accessor: "balance",
+      },
+      {
+        width: "11%",
+        Header: t("bill"),
+        accessor: "billingCycle",
+        Cell: ({ cell: { value } }) => {
+          return moment(value).format("MMM DD YYYY hh:mm A");
+        },
+      },
+    ],
+    []
+  );
+
   return (
     <div
       className="modal fade"
@@ -37,7 +144,7 @@ const CustomerSync = ({
       aria-labelledby="customerModalDetails"
       aria-hidden="true"
     >
-      <div className="modal-dialog ">
+      <div className="modal-dialog modal-xl">
         <div className="modal-content">
           <div className="modal-header">
             <h5
@@ -55,20 +162,36 @@ const CustomerSync = ({
             ></button>
           </div>
           <div className="modal-body">
-            <div class="form-check mt-4">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                checked={inActiveCustomer}
-                id="flexCheckDefault"
-                onChange={(event) => setInActiveCustomer(event.target.checked)}
-              />
-              <label class="form-check-label" for="flexCheckDefault">
-                <small className="text-secondary">
-                  {t("doYouWantToSyncWithInActiveCustomer")}
-                </small>
-              </label>
-            </div>
+            {firstLoading && <div className="">loading...</div>}
+            {loaded && !firstLoading ? (
+              <div class="form-check mt-4">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  checked={inActiveCustomer}
+                  id="flexCheckDefault"
+                  onChange={(event) =>
+                    setInActiveCustomer(event.target.checked)
+                  }
+                />
+                <label class="form-check-label" for="flexCheckDefault">
+                  <small className="text-secondary">
+                    {t("doYouWantToSyncWithInActiveCustomer")}
+                  </small>
+                </label>
+              </div>
+            ) : (
+              <>
+                <Table
+                  columns={column}
+                  data={customer}
+                  isLoading={false}
+                  bulkState={{
+                    setBulkCustomer,
+                  }}
+                ></Table>
+              </>
+            )}
 
             <div className="modal-footer" style={{ border: "none" }}>
               <button
@@ -80,7 +203,7 @@ const CustomerSync = ({
                 {t("cancel")}
               </button>
               <button
-                onClick={syncCostomer}
+                onClick={loaded ? syncCustomer : syncCustomerFirstTime}
                 className="btn btn-success"
                 disabled={isLoading}
               >
