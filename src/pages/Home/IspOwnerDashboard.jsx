f73b0bbb85b2dbc80f7ed845243866155645e419
bashhojaifa <b.hojaifa@gmail.com>
2023-09-19 21:50:44 +0600

    get back new customer calculation in dashboard

--- src/pages/Home/IspOwnerDashboard.jsx
+++ src/pages/Home/IspOwnerDashboard.jsx
@@ -268,7 +268,7 @@ export default function IspOwnerDashboard() {
     if (ispOwnerPermission?.dashboardProbabilityAmountWithNewCustomer) {
       return (
         customerStat.totalProbableAmount -
-        customerStat.totalInactiveAmount +
+        customerStat.totalInactiveAmount -
         customerStat.newCustomerBillCount
       );
     } else {
