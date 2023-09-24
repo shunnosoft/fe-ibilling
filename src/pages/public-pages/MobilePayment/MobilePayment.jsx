c3cc94708e9687ea9cbeef9e366f5639641f1d00
seyam13913 <smmatihassen@gmail.com>
2023-09-24 18:01:27 +0600

    mobile payment done

---
+++ src/pages/public-pages/MobilePayment/MobilePayment.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import { badge } from "../../../components/common/Utils";
+import { getCustomerInfo, getIspOwner } from "../../../features/qrCodeApi";
+import PayMobile from "./PayMobile";
+
+const MobilePayment = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const ispId = urlParams.get("ispId");
+  const customerId = urlParams.get("customerId");
+
+  const [ispInfo, setIspInfo] = useState(null);
+  const [customerInfo, setCustomerInfo] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (customerId) {
+      getIspOwner(ispId, setIsLoading, setIspInfo);
+    }
+  }, [customerId]);
+
+  useEffect(() => {
+    if (ispId && ispInfo) {
+      getCustomerInfo(ispInfo.id, setIsLoading, setCustomerInfo, customerId);
+    }
+  }, [ispInfo]);
+
+  if (ispInfo && customerInfo) {
+    return (
+      <>
+        <ToastContainer theme="colored" />
+        <div className="ispOwner">
+          <div className="w-100 mt-5">
+            <div className="d-flex justify-content-center align-items-center">
+              <div className="card mw-75 p-5">
+                <div className="card-body">
+                  {/* <h4 className=" p-3 text-center">ISP Owner Information</h4>
+
+                  <table className="client_info">
+                    <tr>
+                      <td>Company</td>
+                      <td>{ispInfo?.company}</td>
+                    </tr>
+                    <tr>
+                      <td>Name</td>
+                      <td>{ispInfo.name}</td>
+                    </tr>
+                    <tr>
+                      <td>Mobile</td>
+                      <td>{ispInfo.mobile}</td>
+                    </tr>
+                    <tr>
+                      <td>Email</td>
+                      <td>{ispInfo.email}</td>
+                    </tr>
+                    <tr>
+                      <td>Address</td>
+                      <td>{ispInfo.address}</td>
+                    </tr>
+                  </table>
+                  <hr /> */}
+                  <h5 className="card-title text-center">Payment Info</h5>
+                  <div className="card-text">
+                    <table className="client_info mb-3">
+                      <tr>
+                        <td>Customer Name</td>
+
+                        <td>: {customerInfo.name}</td>
+                      </tr>
+                      <tr>
+                        <td>Customer ID</td>
+                        <td>
+                          <strong>: {customerInfo.customerId}</strong>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Mobile</td>
+                        <td>: {customerInfo?.mobile}</td>
+                      </tr>
+                      <tr>
+                        <td>Monthly Fee</td>
+                        <td>
+                          <strong>: {customerInfo.monthlyFee} TK</strong>{" "}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Current Status</td>
+                        <td>: {badge(customerInfo.status)}</td>
+                      </tr>
+                      <br />
+                      <PayMobile customerData={customerInfo} isPublic />
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return "";
+  }
+};
+
+export default MobilePayment;
---
+++ src/pages/public-pages/MobilePayment/PayMobile.jsx
@@ -0,0 +1,158 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import apiLink, { publicRequest } from "../../../api/apiLink";
+import Loader from "../../../components/common/Loader";
+import { billPayment } from "../../../features/getIspOwnerUsersApi";
+
+const PayMobile = (props) => {
+  const customerData = useSelector(
+    (state) => state.persistedReducer.auth?.currentUser?.customer
+  );
+  const [paymentAmount, setPaymentAmount] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [agreement, setAgreement] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    if (!props.customerData && customerData) {
+      setPaymentAmount(customerData.monthlyFee);
+      setUserData(customerData);
+    }
+    //for handling qr payment
+    if (props.customerData) {
+      setPaymentAmount(props.customerData.monthlyFee); //handle qr payment
+      setUserData(props.customerData);
+    }
+  }, [customerData, props.customerData]);
+
+  useEffect(() => {
+    //window.location.reload();
+  }, []);
+
+  const billPaymentController = async () => {
+    const data = {
+      amount: paymentAmount,
+      name: userData.name,
+      billType: "bill",
+      customer: userData.id,
+      ispOwner: userData.ispOwner.id,
+      user: userData.id,
+      userType: userData.userType,
+      medium: userData.ispOwner.bpSettings?.paymentGateway?.gatewayType,
+      paymentStatus: "pending",
+      mikrotikPackage: userData.mikrotikPackage,
+    };
+
+    if (paymentAmount < userData.monthlyFee) {
+      return alert("You can't pay less than your monthly fee");
+    }
+    billPayment(data, setLoading);
+  };
+
+  const bKash = window.bkash;
+
+  let URL = {
+    create: "bkash/createPayment",
+    execute: "bkash/executePayment",
+    baseURL: apiLink,
+  };
+  if (props.isPublic) {
+    URL = {
+      create: "bkash/createPublicPayment",
+      execute: "bkash/executePublicPayment",
+      baseURL: publicRequest,
+    };
+  }
+
+  useEffect(() => {
+    let paymentID = "";
+    if (userData) {
+      bKash.init({
+        paymentMode: "checkout", //fixed value ‘checkout’
+        paymentRequest: {
+          amount: 1, //paymentAmount
+          merchantInvoiceNumber: Date.now(),
+          intent: "sale",
+          ispOwnerId: userData.ispOwner.id,
+          name: userData.name,
+          billType: "bill",
+          customer: userData.id,
+          user: userData.id,
+          userType: userData.userType,
+          medium: userData.ispOwner.bpSettings?.paymentGateway?.gatewayType,
+          paymentStatus: "pending",
+          collectedBy: "customer",
+        },
+        createRequest: async function (request) {
+          try {
+            const { data } = await URL.baseURL.post(URL.create, request);
+            if (data?.statusCode === "0000") {
+              localStorage.setItem("paymentAmount", paymentAmount);
+              sessionStorage.setItem("qrispid", userData.ispOwner.id);
+              window.location.href = data?.bkashURL;
+            }
+
+            if (data?.paymentID) {
+              paymentID = data.paymentID;
+              bKash.create().onSuccess(data);
+            } else {
+              bKash.create().onError();
+              window.location.href = "/payment/failed";
+            }
+          } catch (error) {
+            bKash.create().onError();
+            window.location.href = "/payment/failed";
+            console.log(error);
+          }
+        },
+        executeRequestOnAuthorization: async function () {
+          const billData = {
+            amount: paymentAmount,
+            name: userData.name,
+            billType: "bill",
+            customer: userData.id,
+            ispOwner: userData.ispOwner.id,
+            user: userData.id,
+            userType: userData.userType,
+            medium: userData.ispOwner.bpSettings?.paymentGateway?.gatewayType,
+            paymentStatus: "pending",
+            mikrotikPackage: userData.mikrotikPackage,
+          };
+          try {
+            const { data } = await URL.baseURL.post(
+              `${URL.execute}?paymentID=${paymentID}`,
+              billData
+            );
+            console.log(data);
+            if (data.bill.paymentStatus === "paid") {
+              window.location.href = "/payment/success";
+            } else {
+              window.location.href = "/payment/failed";
+              bKash.execute().onError();
+            }
+          } catch (error) {
+            bKash.execute().onError();
+            window.location.href = "/payment/failed";
+            console.log(error);
+          }
+        },
+      });
+    }
+  }, [userData, paymentAmount]);
+
+  const gatewayType =
+    userData?.ispOwner?.bpSettings?.paymentGateway?.gatewayType;
+
+  return (
+    <button
+      id={gatewayType === "bKashPG" ? "bKash_button" : ""}
+      onClick={gatewayType !== "bKashPG" ? billPaymentController : () => {}}
+      type="button"
+      className="btn btn-sm btn-success  shadow-none"
+    >
+      {loading ? <Loader /> : "Pay"}
+    </button>
+  );
+};
+
+export default PayMobile;
